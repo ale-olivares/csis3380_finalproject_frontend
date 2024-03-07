8496730e34f7553ef0c927cdc795f99a5308d26a
Luis Miguel Miranda <82402207+luismirandad27@users.noreply.github.com>
2024-03-07 13:33:10 -0800

    Adding commit on App jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,14 +3,40 @@ import reactLogo from '../assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ProductDetailComponent from '../components/product/ProductDetailComponent'
+import Navbar from "../components/Navbar";
+import Home from "../components/Home";
+import Menu from "../components/Menu";
+import About from "../components/About";
+import Product from "../components/Product";
+import Footer from "../components/Footer";
 
 function App() {
-  
+
   return (
     <>
-      <div>
-        <ProductDetailComponent productId="65e8b8119a2ec477d4239219" userId="65e8b89bfeef1c905fe9e184"/>
-      </div>
+      <Navbar />
+
+      <main>
+        <div id="home">
+          <Home />
+        </div>
+
+        <div id="home">
+          <Menu />
+        </div>
+
+        <div id="About us">
+          <About />
+        </div>
+
+        <div>
+          <ProductDetailComponent productId="65e8b8119a2ec477d4239219" userId="65e8b89bfeef1c905fe9e184" />
+        </div>
+
+      </main>
+
+      <Footer />
+
     </>
   )
 }
