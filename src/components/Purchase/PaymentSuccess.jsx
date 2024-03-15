6057e8353d5c6360a5892dd8b546484d303cf8f9
Luis Miguel Miranda <82402207+luismirandad27@users.noreply.github.com>
2024-03-15 00:28:03 -0700

    Adjusting the Review Card and Payment Success/Canceled components

--- src/components/Purchase/PaymentSuccess.jsx
+++ src/components/Purchase/PaymentSuccess.jsx
@@ -2,14 +2,17 @@ import React, { useEffect, useState } from "react";
 import { getCurrentUser } from "../../services/AuthService";
 import { getCart as getCartService, removeCart as removeCartService } from '../../services/CartService';
 import { getStripeSession as getStripeSessionService } from '../../services/PaymentService';
-
 import { saveOrder as saveOrderService } from '../../services/PaymentService';
 import { useNavigate } from "react-router-dom";
+import defaultImageSuccess from '../../assets/img/success.png';
 
 const PaymentSuccessComponent = () => {
 
     const navigate = useNavigate();
     const [orderItems, setOrderItems] = useState({items: []});
+    const [subtotal, setSubtotal] = useState(0);
+    const [taxes, setTaxes] = useState(0);
+    const [total, setTotal] = useState(0);
 
     useEffect(() => {
 
@@ -56,7 +59,7 @@ const PaymentSuccessComponent = () => {
                                         removeCartService(cartItems._id);
                                         
                                         // Set orderItems
-                                        setOrderItems(purchaseOrder);
+                                        setOrderItems(response);
 
                                     }
                                 });
@@ -82,16 +85,114 @@ const PaymentSuccessComponent = () => {
 
     }, []);
 
+    // Format the creation date
+    const formatDate = (dateString) => {
+        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' };
+        return new Date(dateString).toLocaleDateString(undefined, options);
+    };
 
+    const calculateSubtotal = () => {
+        let newSubtotal = 0;
+        
+        if (orderItems && orderItems.items.length > 0){
+            orderItems.items.forEach((item) => {
+                newSubtotal += (parseInt(item.quantity) * parseFloat(item.unit_price.$numberDecimal));
+            });
+        }
+    
+        setSubtotal(newSubtotal);
+    };
+    
+    const calculateTaxes = () => {
+        setTaxes(subtotal * 0.13);
+    };
+    
+    const calculateTotal = () => {
+        setTotal(subtotal + taxes);
+    };
+
+    useEffect(() => {
+        calculateSubtotal();
+    }, [orderItems]);
+
+    useEffect(() => {
+        calculateTaxes();
+    }, [subtotal]);
+
+    useEffect(() => {
+        calculateTotal();
+    }, [subtotal, taxes]);
+
+    if (!(orderItems && orderItems.items)) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <div className="container pt-40 px-40 pb-40 md:mx-auto min-h-screen">
             <div className="text-center">
                 <h1 className="text-4xl font-bold">Payment Success</h1>
-                <p className="text-lg">Your payment has been success</p>
+                <img src={defaultImageSuccess} alt="Success" className="mx-auto mt-6 w-48 h-auto object-cover" />
+                <p className="text-lg text-gray-500 mt-2">Your payment has been successful.</p>
             </div>
+            {orderItems && orderItems.items ? (
+                <div className="mt-10 p-6 bg-white shadow-lg rounded-lg">
+                    <h2 className="text-2xl font-semibold text-gray-800">We received your order!</h2>
+                    <div className="mt-4 text-gray-600">
+                        <p><strong>Order Status:</strong> {orderItems.order_status}</p>
+                        <p><strong>Order Date:</strong> {formatDate(orderItems.created_at)}</p>
+                    </div>
+
+                    <h3 className="text-xl font-semibold mt-6 text-gray-800">Items Ordered:</h3>
+                    <div className="mt-4">
+                        <table className="table-auto w-full text-left">
+                            <thead>
+                                <tr className="bg-gradient-to-r from-gray-200 to-gray-300 rounded-lg">
+                                    <th className="px-4 py-2 rounded-l-lg">Product</th>
+                                    <th className="px-4 py-2">Quantity</th>
+                                    <th className="px-4 py-2">Unit Price</th>
+                                    <th className="px-4 py-2 rounded-r-lg">Subtotal</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {orderItems.items.map((item, index) => (
+                                    <tr key={index} className={`border-b ${index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}`}>
+                                        <td className="px-4 py-2">
+                                            {item.product.name} 
+                                            <br/>
+                                            {item.grind_type.name}
+                                            <br/>
+                                            {item.product_subtype.name}</td>
+                                        <td className="px-4 py-2">{item.quantity}</td>
+                                        <td className="px-4 py-2">${item.unit_price.$numberDecimal}</td>
+                                        <td className="px-4 py-2">${(item.quantity * item.unit_price.$numberDecimal).toFixed(2)}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="flex justify-end mt-10">
+                        <div className="w-1/3">
+                            <div className="flex justify-between">
+                                <span>Subtotal</span>
+                                <span>${subtotal.toFixed(2)}</span>
+                            </div>
+                            <div className="flex justify-between">
+                                <span>Taxes</span>
+                                <span>${taxes.toFixed(2)}</span>
+                            </div>
+                            <div className="flex justify-between">
+                                <span>Total</span>
+                                <span>${total.toFixed(2)}</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : (
+                <h1 className="text-center mt-10 text-xl font-semibold text-red-500">No order details available.</h1>
+            )}
         </div>
-    )
+    );
 }
 
 export default PaymentSuccessComponent;
