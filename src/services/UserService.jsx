0fdf06debff711849ee8b4768013eac7f4eb2309
Andrea Olivares <alejandraolivares06@gmail.com>
2024-03-28 19:03:29 -0700

    Adding auth header

--- src/services/UserService.jsx
+++ src/services/UserService.jsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import authHeader from './AuthHeader';
 
 const BASE_URL = import.meta.env.VITE_BACKEND_API_URL;
 
@@ -6,7 +7,10 @@ export const getUsers = async (filters = {}) => {
     try{
         const queryString = new URLSearchParams(filters).toString();
         const url = queryString ? `${BASE_URL}/users?${queryString}` : `${BASE_URL}/users`;
-        const response = await axios.get(url);
+        const response = await axios.get(url, 
+        {
+            headers: { ...authHeader()}
+        });
         return response.data;
     }
     catch(error){
